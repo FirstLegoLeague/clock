eec1143157d43a33e3e0657c0281c5ad60158524
rikkertkoppes <rikkert@rikkertkoppes.com>
2013-10-04 20:39:56 +0200

    refactored most of the stuff to angular. Sounds needs work

--- js/ClockCtrl.js
+++ js/ClockCtrl.js
@@ -106,6 +106,171 @@ var FontFitter = (function() {
     return FontFitter;
 }());
 
+angular.module('Clock',[])
+    .filter('time',function() {
+        function pad(str) {
+            str = ''+str;
+            if (str.length<2) {
+                str = '0'+str;
+            }
+            return str;
+        }
+        return function(msec,hundreds) {
+            var secs = Math.floor(msec/1000);
+            var hsec = Math.floor((msec/100)) % 10;
+            var sec = secs % 60;
+            var min = Math.floor((secs-sec)/60);
+            var str = min+':'+pad(sec);
+            if (hundreds) {
+                str += '.'+hsec;
+            }
+            return str;
+        };
+    })
+    .controller('ClockCtrl',function($scope,$timeout) {
+        $scope.bgColor = 'black';
+        $scope.state = 'stopped';
+        $scope.time = 150000;
+        $scope.armTime = 150;
+        $scope.hundreds = false;
+        $scope.size = 340;
+        $scope.pos = {
+            x: 0,
+            y: 0
+        };
+
+        $scope.arm = function() {
+            $scope.time = $scope.armTime*1000;
+            $scope.state = 'armed';
+        };
+
+        $scope.start = function() {
+            $scope.startTime = +(new Date());
+            $scope.state = 'started';
+            $scope.tick();
+        };
+
+        $scope.stop = function() {
+            $scope.state = 'stopped';
+        };
+
+        $scope.toggle = function() {
+            if ($scope.state == 'started') {
+                $scope.stop();
+            } else {
+                $scope.start();
+            }
+        };
+
+        $scope.mode = function() {
+            $scope.hundreds = !$scope.hundreds;
+        };
+
+        $scope.tick = function() {
+            if ($scope.state === 'started') {
+                var now = +(new Date());
+                $scope.time = ($scope.armTime*1000) - (now - $scope.startTime);
+                $timeout($scope.tick,10);
+            }
+        };
+
+        $scope.$watch('time',function(newVal) {
+            if ($scope.time <= 0) {
+                $scope.time = 0;
+                $scope.stop();
+            }
+        });
+
+        $scope.clockStyle = function() {
+            return {
+                fontSize: $scope.size,
+                left: $scope.pos.x+'px',
+                top: $scope.pos.y+'px'
+            };
+        };
+
+        $scope.bodyStyle = function() {
+            return {
+                backgroundColor: $scope.bgColor
+            };
+        };
+
+        angular.element(document).bind('keydown',function(e) {
+            var key = e.which;
+            console.log(key);
+            switch(key) {
+                case 65:    //a
+                    $scope.arm();
+                    break;
+                case 88:    //x
+                    $scope.stop();
+                    break;
+                case 83:    //s
+                case 32:    //space
+                    $scope.toggle();
+                    break;
+                case 219:   //[
+                    $scope.size -= 1;
+                    break;
+                case 221:   //]
+                    $scope.size += 1;
+                    break;
+                case 72:    //h
+                    $scope.mode();
+                    break;
+                case 38:    //up
+                    $scope.pos.y -= 1;
+                    break;
+                case 40:    //down
+                    $scope.pos.y += 1;
+                    break;
+                case 37:    //left
+                    $scope.pos.x -= 1;
+                    break;
+                case 39:    //right
+                    $scope.pos.x += 1;
+                    break;
+                case 77:    //m
+                    $scope.state = 'edit';
+                    break;
+                case 13:    //enter
+                    if ($scope.state == 'edit') {
+                        $scope.arm();
+                    }
+                    break;
+            }
+            $scope.$apply();
+        });
+
+        if (window.io) {
+            var socket = io.connect('http://io.example.com:1390/');
+            socket.on('clock', function (data) {
+                var bd = data.cmd||null;
+                switch (bd) {
+                    case 'color':
+                        $scope.bgColor = data.color;
+                        break;
+                    case 'arm':
+                        $scope.arm(data.countdown);
+                        break;
+                    case 'start':
+                        $scope.start(null,data.countdown);
+                        break;
+                    case 'stop':
+                        $scope.stop();
+                        break;
+                    case 'nudge':
+                        $scope.pos[data.direction] += data.amount;
+                        break;
+                    case 'size':
+                        $scope.size += data.amount;
+                        break;
+                }
+                $scope.$apply();
+            });
+        }
+    });
+
 var trackA, trackB;
 function initAudio () {
     trackA = new Audio5js({
