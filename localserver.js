f517eb45cbdb21568bbb3ba0cca6a9934ae7612f
Idan Stark <idanstark42@gmail.com>
2017-08-10 17:37:08 +0300

    Added librarreading option in order to find all mp3 files in a genric way

--- localserver.js
+++ localserver.js
@@ -1,6 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var serveIndex = require('serve-index');
 var port = 1392;
-connect().use(serveStatic(__dirname)).listen(port);
+connect()
+	.use(serveStatic(__dirname))
+	.use('/mp3', serveIndex(__dirname + '/mp3'))
+	.listen(port);
 console.log('server running on port',port);
 console.log('open browser to http://localhost:'+port+'/');
