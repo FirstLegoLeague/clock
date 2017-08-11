3c5a3f73cf5bccd58f2c66a113f04dfb47520e10
Idan Stark <idanstark42@gmail.com>
2017-08-11 16:41:06 +0300

    Set default configuration to what it was

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var clockConfig = {
     seconds: 150,
     tracks: [{
     	source: 'mp3/lossetrack-A +6.mp3',
-    	start: '5s after start'
+    	start: 'on start'
     },{
     	source: 'mp3/lossetrack-B.mp3',
     	start: 'on end'
