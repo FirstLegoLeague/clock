eb4895d509453713d6e10b428804abdf7d826153
Roy Shmeuli <roysh9@gmail.com>
2018-04-09 14:59:12 +0300

    Adding modes and configurations

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ]
+})
