0d160f69ee6f42c058cd84d969e1bc207d007dc5
Tal Taub <taltaub22@gmail.com>
2018-11-11 20:13:43 +0200

    - Added error handler - Added logs

--- server/manager.js
+++ server/manager.js
@@ -4,6 +4,8 @@ const ENDED = 'ended'
 
 const EventEmitter = require('events')
 
+const { logger } = require('./logger')
+
 const WRONG_STATE_OF_CLOCK_CODE = exports.WRONG_STATE_OF_CLOCK_CODE = 'WRONG_STATE_OF_CLOCK'
 
 const MATCH_TIME = (process.env.NODE_ENV === 'development') ? 35 : 150
@@ -72,22 +74,30 @@ exports.ClockManager = class extends EventEmitter {
       })
     }
 
-    this._timeSaver.getTime().then(time => {
-      if (time) {
-        this._status = RUNNING
+    this._timeSaver.getTime()
+      .then(time => {
+        if (time) {
+          this._status = RUNNING
 
-        const currentTime = new Date()
-        const timeRan = Math.floor((currentTime.getTime() - time.getTime()) / 1000)
-        const matchTimeWithBuffer = MATCH_TIME + MATCH_TIME_BUFFER
+          const currentTime = new Date()
+          const timeRan = Math.floor((currentTime.getTime() - time.getTime()) / 1000)
 
-        if (timeRan < matchTimeWithBuffer) {
-          const timeLeft = matchTimeWithBuffer - timeRan
-          this._clock.startCountdown(timeLeft)
-        } else {
-          this._clock.startCountdown(0)
+          if (timeRan < MATCH_TIME) {
+            const timeLeft = MATCH_TIME - timeRan
+            logger.info(`Running timer after restart. Time Left: ${timeLeft}`)
+            this._clock.startCountdown(timeLeft)
+          } else {
+            logger.info('Clock recoverd after time is up.')
+            if (timeRan < MATCH_TIME + MATCH_TIME_BUFFER) {
+              logger.info('We are in the buffer period. Playing end sound.')
+              this._clock.startCountdown(0)
+            }
+          }
         }
-      }
-    })
+      }).catch(err => {
+        logger.error(err.message)
+        this._clock.startCountdown(0)
+      })
 
     this._status = ARMED
     this._clock.setTime(MATCH_TIME)
