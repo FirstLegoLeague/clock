7fbe1bd77ae83f4e5b4718b685411fab59f6617a
Alan Green <alanggreen@gmail.com>
2018-11-12 13:31:49 +0200

    Handle missing time-save file and other fixes

--- server/time-saver.js
+++ server/time-saver.js
@@ -6,7 +6,7 @@ const writeFile = Promise.promisify(fs.writeFile)
 const readFile = Promise.promisify(fs.readFile)
 
 const DATA_DIR = process.env.DATA_DIR
-const FILE_NAME = 'timer-start-time.json'
+const FILE_NAME = 'timer-start-time.txt'
 const FILE_PATH = path.join(DATA_DIR, FILE_NAME);
 
 exports.TimeSaver = class {
@@ -15,16 +15,26 @@ exports.TimeSaver = class {
   }
 
   getTime () {
-    return readFile(FILE_PATH, 'utf-8').then(time => {
-      time = JSON.parse(time)
-      if (time) {
-        return new Date(parseInt(time))
-      }
-      return null
-    })
+    return readFile(FILE_PATH, 'utf-8')
+      .then(time => {
+        time = JSON.parse(time)
+        if (time) {
+          return new Date(parseInt(time))
+        }
+        return null
+      })
+      .catch(err => {
+        if (err.code != "ENOENT") {
+          logger.warn(`Error reading TimeSaver file ${FILE_PATH}`)
+          throw err
+        }
+        else {
+          return null
+        }
+      })
   }
 
-  clearTime () {
+  clearTime() {
     return writeFile(FILE_PATH, null)
   }
 }
