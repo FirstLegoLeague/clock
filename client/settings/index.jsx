e5ccf98ddc93e188f7f26744edeed1e59cb4eceb
Roy <2903801+2roy999@users.noreply.github.com>
2019-06-07 19:18:40 +0300

    Adding sounds to the client side

--- client/settings/index.jsx
+++ client/settings/index.jsx
@@ -1,7 +1,4 @@
 
-import axios from 'axios'
-import Promise from 'bluebird'
-import Modal from 'react-foundation-modal'
 import React, { Component } from 'react'
 
 import './index.css'
@@ -9,50 +6,29 @@ import './index.css'
 export default class Settings extends Component {
   constructor (props) {
     super(props)
+
+    window.onstorage = event => {
+      if (event.key === 'sound-window') {
+        this.setState({ sound: Boolean(event.newValue) })
+      }
+    }
+
     this.state = {
-      modalIsOpen: false
+      sound: Boolean(window.localStorage.getItem('sound-window'))
     }
   }
 
-  testSound (sound) {
-    Promise.resolve(axios.post(`/api/sound/${sound}`))
-      .catch(e => {
-        console.error(e)
-      })
-  }
-
-  showPopup (status) {
-    this.setState({
-      modalIsOpen: status
-    })
+  openSoundWindow () {
+    if (!this.state.sound) {
+      window.open('/sound.html', 'sound-window', 'height=200,width=700')
+    }
   }
 
   render () {
     return (<div className='show-on-hover settings'>
-      <button className='large clear button' onClick={() => this.showPopup(true)}>
-        <i className='fas fa-music' />
+      <button className='large clear button' onClick={() => this.openSoundWindow()} disabled={this.state.sound}>
+        <i className={`fas fa-volume-${this.state.sound ? 'up' : 'off'}`} />
       </button>
-      <Modal
-        open={this.state.modalIsOpen}
-        closeModal={() => this.showPopup(false)}
-        isModal
-        size='large'>
-        <h1>Test sounds</h1>
-        <div className={'large expanded button-group'}>
-          <button className='button' type='button' onClick={() => this.testSound('start')} >
-            Start Sound
-          </button>
-          <button className='button' type='button' onClick={() => this.testSound('end')} >
-            End Sound
-          </button>
-          <button className='button' type='button' onClick={() => this.testSound('stop')} >
-            Stop Sound
-          </button>
-          <button className='button' type='button' onClick={() => this.testSound('endgame')} >
-            End Game Sound
-          </button>
-        </div>
-      </Modal>
     </div>)
   }
 }
