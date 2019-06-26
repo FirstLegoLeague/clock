566fa134ffd05da200d1fdf1057eb6cd63c2ac3b
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-06-26 15:50:50 +0300

    Fixing build process

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@
 
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const common = require('./webpack.common.js')
 
