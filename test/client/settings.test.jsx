154ea8243145a98609f137cf5353c1452dde7d94
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-07-03 10:35:40 +0300

    Adding tests for settings component

---
+++ test/client/settings.test.jsx
@@ -0,0 +1,83 @@
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+import Settings from '../../client/settings/index.jsx'
+
+Enzyme.configure({ adapter: new Adapter() })
+chai.use(chaiEnzyme())
+chai.use(sinonChai)
+
+const { mount } = Enzyme
+const { expect } = chai
+
+describe('Settings Component', () => {
+  let onStorageListener
+
+  beforeEach(() => {
+    window.localStorage.clear()
+
+    const onStorageStub = sinon.stub(window, 'onstorage')
+      .get(() => onStorageStub)
+      .set(fn => {
+        onStorageListener = fn
+      })
+
+    sinon.stub(window, 'open')
+  })
+
+  afterEach(() => {
+    window.onstorage.restore()
+    window.open.restore()
+  })
+
+  it('shows the sound off icon when created without window-sound key in the local storage', () => {
+    const wrapper = mount(<Settings />)
+
+    expect(wrapper.find('.fas')).to.have.className('fa-volume-off')
+  })
+
+  it('shows the sound up icon when created with window-sound key in the local storage', () => {
+    window.localStorage.setItem('sound-window', true)
+    const wrapper = mount(<Settings />)
+
+    expect(wrapper.find('.fas')).to.have.className('fa-volume-up')
+  })
+
+  it('changes the icon when window-sound appears in the local storage', () => {
+    const wrapper = mount(<Settings />)
+
+    onStorageListener({ key: 'sound-window', newValue: 'true' })
+
+    expect(wrapper.find('.fas')).to.have.className('fa-volume-up')
+  })
+
+  it('changes the icon when window-sound removed from the local storage', () => {
+    window.localStorage.setItem('sound-window', true)
+    const wrapper = mount(<Settings />)
+
+    onStorageListener({ key: 'sound-window', oldValue: 'true' })
+
+    expect(wrapper.find('.fas')).to.have.className('fa-volume-off')
+  })
+
+  it('does nothing when local storage changed on other key then window-sound', () => {
+    const wrapper = mount(<Settings />)
+
+    onStorageListener({ key: 'other-key', newValue: 'true' })
+
+    expect(wrapper.find('.fas')).to.have.className('fa-volume-off')
+  })
+
+  it('opens a new window when icon has been clicked', () => {
+    const wrapper = mount(<Settings />)
+
+    wrapper.find('.fas').simulate('click')
+
+    expect(window.open).to.have.calledWith('/sound.html')
+  })
+})
