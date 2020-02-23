2179b2dc3d8da7054e35b49ab73f8c1d9345f912
Idan Stark <idanstark42@gmail.com>
2020-02-23 22:24:58 +0200

    new buttons that span over the entire window and cannot be confused with double clicks

--- client/controls/index.jsx
+++ client/controls/index.jsx
@@ -1,83 +1,109 @@
-
 import axios from 'axios'
-import Promise from 'bluebird'
 import React, { Component } from 'react'
 
 import './index.css'
 
+const NUMBERS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
+
+// This prevent double-clicks from pressing on the button, making an unintentional start/stop of a match
+const TIMEOUT_WITHOUT_BUTTONS = 100
+
 export default class Controls extends Component {
+  constructor () {
+    super()
+    this.state = { hideButtons: false }
+  }
+
   prestartClock () {
-    Promise.resolve(axios.post('/api/action/prestart'))
+    axios.post('/api/action/prestart')
+      .then(() => this.setTimeoutWithoutButtons())
       .catch(e => {
         console.error(e)
       })
   }
 
   startClock () {
-    Promise.resolve(axios.post('/api/action/start'))
+    axios.post('/api/action/start')
+      .then(() => this.setTimeoutWithoutButtons())
       .catch(e => {
         console.error(e)
       })
   }
 
   stopClock () {
-    Promise.resolve(axios.post('/api/action/stop'))
+    axios.post('/api/action/stop')
+      .then(() => this.setTimeoutWithoutButtons())
       .catch(e => {
         console.error(e)
       })
   }
 
   reloadClock () {
-    Promise.resolve(axios.post('/api/action/reload'))
+    axios.post('/api/action/reload')
+      .then(() => this.setTimeoutWithoutButtons())
       .catch(e => {
         console.error(e)
       })
   }
 
+  setTimeoutWithoutButtons () {
+    this.setState({ hideButtons: true })
+    setTimeout(() => {
+      this.setState({ hideButtons: false })
+    }, TIMEOUT_WITHOUT_BUTTONS)
+  }
+
   button (color, callback, icon, label) {
-    return <div className='controls' onClick={callback}>
-      <i className={`ui ${icon} ${color} link huge icon`} />
-      <div className={`hover text ui ${color} header`}>{label}</div>
+    return <div className={`ui ${color} icon button`} onClick={callback}>
+      <i className={`ui ${icon} icon`} />
+      {label}
     </div>
   }
 
   startButton () {
-    return this.button('green', this.startClock, 'play', 'Start')
+    return this.button('green', () => this.startClock(), 'play', 'Start')
   }
 
   prestartButton () {
-    return this.button('orange', this.prestartClock, 'stopwatch', 'Start after delay')
+    return this.button('orange', () => this.prestartClock(), 'stopwatch', 'Start after delay')
   }
 
   stopButton () {
-    return this.button('red', this.stopClock, 'stop', 'Abort')
+    return this.button('red', () => this.stopClock(), 'stop', 'Abort')
   }
 
   reloadButton () {
-    return this.button('orange', this.reloadClock, 'undo', 'Reload')
+    return this.button('orange', () => this.reloadClock(), 'undo', 'Reload')
   }
 
   renderButtons () {
+    if (this.state.hideButtons) {
+      return []
+    }
+
     switch (this.props.status) {
       case 'armed':
         if (this.props.countdownEnabled) {
           return [this.startButton(), this.prestartButton()]
         } else {
-          return this.startButton()
+          return [this.startButton()]
         }
       case 'prerunning':
         return [this.stopButton()]
       case 'running':
-        return this.stopButton()
+        return [this.stopButton()]
       case 'ended':
-        return this.reloadButton()
+        return [this.reloadButton()]
     }
-    return null
+    return []
   }
 
   render () {
-    return <div className='controls-container show-on-hover'>
-      {this.renderButtons()}
+    const buttons = this.renderButtons()
+    return <div className='controls-container show-on-hover ui buttons'>
+      <div className={`ui ${NUMBERS[buttons.length]} huge buttons`}>
+        {buttons}
+      </div>
     </div>
   }
 }
